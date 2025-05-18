f418e8e1c7e0d24c097c69efc89a6e89b3161acb
Sumit Modak <sumitpsm@noreply.codeberg.org>
2025-05-18 15:44:15 +0530

    added ytdlp, changed hypr configs

--- nixos/wayland.nix
+++ nixos/wayland.nix
@@ -5,6 +5,15 @@ let
     url = "https://github.com/ful1e5/notwaita-cursor/releases/download/v1.0.0-alpha1/Notwaita-Black.tar.xz";
     sha256 = "0byiix6pda7ibjpc1an1lrnm19prjmqx1q72ipx5q7dijw5z9fk4";
   };
+  ytdlp-github = pkgs.yt-dlp.overrideAttrs (oldAttrs: {
+    version = "2025.04.30";
+    src = pkgs.fetchFromGitHub {
+      owner = "yt-dlp";
+      repo = "yt-dlp";
+      rev = "2025.04.30";
+      sha256 = "sha256-vsMWzZu+kxlxYT5Cq+diNApzE3Cg22Hg0j9eDKLowWI=";
+    };
+  });
 in
 {
   # Sound
@@ -47,7 +56,10 @@ in
 
       # gtk-themes
       andromeda-gtk-theme
-      nordzy-icon-theme
+      # nordzy-icon-theme
+
+      # yt-dlp
+      ffmpeg ytdlp-github
 
       libnotify # notification client
       # pavucontrol, playerctl
