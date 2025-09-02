a1027dcad61632383e945ab583bd53c0293aab39
Sumit Modak <sumitpsm@noreply.codeberg.org>
2025-09-02 17:42:21 +0530

    fixed rust toolchain problem, flake update, keybinds update

--- init.sh
+++ init.sh
@@ -95,14 +95,3 @@ if [[ $INPUT == "y" || $INPUT == "Y" ]]; then
   mkdir -p ~/media/wallpapers
   git clone --depth=1 https://github.com/sumit-modak/wallpapers.git ~/media/wallpapers
 fi
-
-# installing latest rust components
-# if [[ $(nix-store -q --requisites /run/current-system ~/.nix-profile | grep rustup | cut -d - -f2) = "rustup" ]]; then
-#   if [[ $(rustup toolchain list | grep stable | cut -d - -f1) = "stable" ]]; then
-#     rustup update
-#   else
-#     rustup toolchain install stable
-#     rustup default stable
-#     rustup component add rust-analyzer
-#   fi
-# fi
--- modules/dev.nix
+++ modules/dev.nix
@@ -1,41 +1,29 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 
 {
-  # nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
   programs.nano.enable = false;
 
   environment.systemPackages = with pkgs; [
-    # development utilities
     helix git yazi btop # gh
     zoxide eza ripgrep unzip
     bash nixd vscode-langservers-extracted
     # asciinema asciinema-agg presenterm
 
-    rustup gcc pkg-config openssl
-    # # rust toolchains
-    # (rust-bin.stable.latest.default.override {
-    #   extensions = [
-    #     "rust-src"
-    #     "rust-analyzer"
-    #   ];
-    #   targets = [
-    #     "x86_64-unknown-linux-gnu"
-    #     "wasm32-unknown-unknown"
-    #   ];
-    # })
-    # (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+    gcc pkg-config openssl
+    rustc cargo clippy rustfmt rust-analyzer
   ];
+
   environment.sessionVariables = {
     EDITOR = "hx";
     VISUAL = "hx";
     SUDO_EDITOR = "hx";
-    PATH = [ "$HOME/.cargo/bin" ];
 
     # Rust specific environment variables
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-    # RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default}/lib/rustlib/src/rust/library";
-    # RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
     RUST_BACKTRACE = 1;
+    PATH = [ "$HOME/.cargo/bin" ];
   };
+
   environment.variables = {};
 }
